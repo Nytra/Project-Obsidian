2451de16270de0fc9a0650fabadf48c9edb3663d
Nytra <14206961+Nytra@users.noreply.github.com>
2025-05-22 19:15:39 +0100

    Add fork node

---
+++ ProjectObsidian/ProtoFlux/Flow/ForkNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Flow
+{
+    [NodeCategory("Obsidian/Flow")]
+    [NodeName("Fork", false)]
+    public class ForkNode : ActionNode<FrooxEngineContext>
+    {
+        public Call Fork;
+
+        public Continuation OnDone;
+
+        protected override IOperation Run(FrooxEngineContext context)
+        {
+            context.Group.ExecuteImmediatelly(default(NodeContextPath), (c) => 
+            { 
+                Fork.Execute(c);
+            });
+            return OnDone.Target;
+        }
+    }
+}
