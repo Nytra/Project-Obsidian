aa59a4fc088ed97336c729fc80cfe608b1147fa4
Nytra <14206961+Nytra@users.noreply.github.com>
2025-09-24 11:22:38 +0100

    Change initialization to be done by the PluginSettings component

---
+++ ProjectObsidian/Initialization/ObsidianInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elements.Core;
+using FrooxEngine;
+using Obsidian.Components.Tools;
+using Obsidian.Shaders;
+
+namespace Obsidian;
+
+public static class ObsidianInitializer
+{
+    private static bool _initialized = false;
+    public static void Initialize()
+    {
+        if (_initialized) return;
+
+        ShaderInjection.AppendShaders();
+        DevCreateNewForm.AddAction("Obsidian", "MeshEditTool", (slot) =>
+        {
+            if (!slot.World.Types.IsSupported(typeof(MeshEditTool)))
+            {
+                NotificationMessage.SpawnTextMessage("Obsidian is not enabled in this world!", colorX.Red);
+                slot.Destroy();
+                return;
+            }
+            slot.AttachComponent<MeshEditTool>();
+        });
+
+        _initialized = true;
+    }
+}
